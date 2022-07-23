81492e14c41c18baf1b50632682fb0172f6fcb31
juuuuung <pandanam987@gmail.com>
2022-07-24 02:35:22 +0900

    Algorithm/Problem_3052 Comparable CompareTo

--- src/baekjoon/problem/Problem_3052.java
+++ src/baekjoon/problem/Problem_3052.java
@@ -1,15 +1,66 @@
 package baekjoon.problem;
 
+import java.util.List;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collections;
 
-public class Problem_3052{
-    public static void main(String[] args) {
-        /*
-        * 두 자연수 A와 B가 있을 때, A%B는 A를 B로 나눈 나머지 이다. 
-        *예를 들어, 7, 14, 27, 38을 3으로 나눈 나머지는 1, 2, 0, 2이다. 
-        *수 10개를 입력받은 뒤, 이를 42로 나눈 나머지를 구한다. 
-        *그 다음 서로 다른 값이 몇 개 있는지 출력하는 프로그램을 작성하시오.
-        */
-        
-    }
+interface Game{
+	String GetName();
+	int GetLevel();
 }
 
+class GameUser implements Game, Comparable<GameUser>{
+	static int Cnt = 1;
+	String name;
+	int level;
+	int Mynumber;
+
+	public GameUser(String name, int level){
+		this.name = name;
+		this.level = level;
+		this.Mynumber = Cnt++;
+	}
+
+	public String GetName(){
+		return this.name;
+	}
+
+	public int GetLevel(){
+		return this.level;
+	}
+
+	public int GetNumber(){
+		return this.Mynumber;
+	}
+
+	//compareTo
+	public int compareTo(GameUser o){
+		return ((Integer)level).compareTo(o.level);
+	}
+}
+
+
+public class Problem_3052 {
+	public static void main(String[] args) {
+		List<GameUser> list = new ArrayList<>();
+
+		GameUser g1 = new GameUser("tom", 73);
+		GameUser g2 = new GameUser("jisu", 92);
+		GameUser g3 = new GameUser("hoal", 38);
+		GameUser g4 = new GameUser("jirala", 102);
+		GameUser g5 = new GameUser("kala", 62);
+
+		list.add(g1);
+		list.add(g2);
+		list.add(g3);
+		list.add(g4);
+		list.add(g5);
+
+		Collections.sort(list);
+
+		for(GameUser s : list){
+			System.out.printf("%d번 %s %d\n",s.Mynumber ,s.getClass().getSimpleName(),s.level);
+		}
+	}
+}
