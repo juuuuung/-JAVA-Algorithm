608162d5ca46be3c2872804f88f88b3e4b30de42
juuuuung <pandanam987@gmail.com>
2022-07-25 13:45:07 +0900

    Algoritm BaekJoon

---
+++ src/baekjoon/problem/Problem_4344.java
@@ -0,0 +1,7 @@
+package baekjoon.problem;
+
+public class Problem_4344{
+    public static void main(String[] args){
+    
+    }
+}
---
+++ src/baekjoon/problem/Problem_8958.java
@@ -0,0 +1,22 @@
+package baekjoon.problem;
+
+import java.io.*;
+
+public class Problem_8958{
+    public static void main(String[] args) throws IOException{
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int N = Integer.parseInt(br.readLine());
+        int[] score = new int[N];
+        
+        for(int i = 0; i < N; i++){
+            String text = br.readLine();
+            if(text.length() <= 80){
+                String[] ox = text.split("X");
+                for(String s : ox)
+                    if(s.startsWith("O")) score[i] += ((s.length())*(s.length()+1))/2;
+            } else break;
+        }
+        br.close();
+        for(int s : score)System.out.println(s);
+    }
+}
