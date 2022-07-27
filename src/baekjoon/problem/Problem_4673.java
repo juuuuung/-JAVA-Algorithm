933cf4c00b3e6e73c97c58beb366e2633a7f0b19
juuuuung <pandanam987@gmail.com>
2022-07-27 11:39:01 +0900

    함수 Algorithm

---
+++ src/baekjoon/problem/Problem_4673.java
@@ -0,0 +1,31 @@
+package baekjoon.problem;
+
+public class Problem_4673 {
+    public static void main(String[] args) {
+        final int Max = 10000;
+
+        boolean[] arr = new boolean[Max];
+
+        for(int i =1; i <= Max; i++){
+            int num = d(i);
+            if(num < Max){
+                arr[num] = true;
+            }
+        }
+        StringBuilder strbuilder = new StringBuilder();
+        for(int i =1; i < Max; i++){
+            if(!arr[i])
+                strbuilder.append(i).append("\n");
+            }
+        System.out.println(strbuilder.toString());
+    }
+
+    public static int d(int n){
+        int num = n;
+        while(n != 0){
+            num += (n%10);
+            n /= 10;
+        }
+        return num;
+    }
+}
